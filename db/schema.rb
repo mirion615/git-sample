167612518d3eaff64eb04193983bf1128f8c4930
mirion615 <mirion615@gmail.com>
2021-06-28 17:02:46 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_034358) do
+ActiveRecord::Schema.define(version: 2021_06_28_075630) do
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_06_21_034358) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
