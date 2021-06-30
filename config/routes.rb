f314fbeb189ac82775ab2c7a67f4325630c61eb4
takeuchitakumi <tahaizhunei616@gmail.com>
2021-06-30 17:36:58 +0900

    ユーザー管理機能ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-
-  #get 'posts/index'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
-    end
+  end
   devise_for :users
  root to: "posts#index"
-
 end
